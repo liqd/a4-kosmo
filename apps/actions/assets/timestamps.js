liqd/a4-kosmo

commit b2b4192cb2235b0c1b2ff9b060f850e6976e2ddc

assets/timestamps.js: Use minified version of timeago

apps/actions/assets/timestamps.js
@@ -1,5 +1,4 @@
-const timeago = require('timeago.js')
-require('timeago.js/src/timeago.locales.js')
+const timeago = require('timeago.js/dist/timeago.min.js')
 const $ = require('jquery')
 
 $(function () {

commit 4fdf30ba01cb50c5ef827ab7fecaef79230399a8

apps/actions: Fix timeago call
This broke in b2b4192cb2235b0 or the commit before.

apps/actions/assets/timestamps.js
@@ -7,10 +7,9 @@ $(function () {
   $times.each((i, e) => {
     const sevenDays = 60 * 60 * 24 * 7 * 1000
     const datetime = new Date($(e).attr('datetime'))
-    const timeagoInstance = timeago()
 
     if ((new Date() - datetime) < sevenDays) {
-      e.textContent = timeagoInstance.format(datetime, document.documentElement.lang)
+      e.textContent = timeago.format(datetime, document.documentElement.lang)
     }
   })
 })