liqd/adhocracy-plus

tests: More adoptions

tests/debate/rules/test_rules_comment.py
@@ -1,6 +1,7 @@
 import pytest
 import rules
 
+from adhocracy4.projects.models import Access
 from apps.debate import phases
 from tests.helpers import freeze_phase
 from tests.helpers import freeze_post_phase
@@ -52,7 +53,8 @@ def test_phase_active_project_private(phase_factory, user, user2,
                                       member_factory, subject_factory):
     phase, _, project, item = setup_phase(phase_factory, subject_factory,
                                           phases.DebatePhase,
-                                          module__project__is_public=False)
+                                          module__project__access=Access.
+                                          PRIVATE)
     anonymous, moderator, initiator = setup_users(project)
     member = member_factory(organisation=project.organisation)
 

tests/mapideas/rules/test_rules_comment.py
@@ -1,6 +1,7 @@
 import pytest
 import rules
 
+from adhocracy4.projects.models import Access
 from apps.mapideas import phases
 from tests.helpers import freeze_phase
 from tests.helpers import freeze_post_phase
@@ -52,7 +53,8 @@ def test_phase_active_project_private(phase_factory, user, user2,
                                       member_factory, map_idea_factory):
     phase, _, project, item = setup_phase(phase_factory, map_idea_factory,
                                           phases.CollectPhase,
-                                          module__project__is_public=False)
+                                          module__project__access=Access.
+                                          PRIVATE)
     anonymous, moderator, initiator = setup_users(project)
     member = member_factory(organisation=project.organisation)
 

tests/mapideas/rules/test_rules_view.py
@@ -1,6 +1,7 @@
 import pytest
 import rules
 
+from adhocracy4.projects.models import Access
 from apps.mapideas import phases
 from tests.helpers import freeze_phase
 from tests.helpers import freeze_post_phase
@@ -52,7 +53,8 @@ def test_phase_active_project_private(phase_factory, map_idea_factory,
                                       user, user2, member_factory):
     phase, _, project, item = setup_phase(phase_factory, map_idea_factory,
                                           phases.CollectPhase,
-                                          module__project__is_public=False)
+                                          module__project__access=Access.
+                                          PRIVATE)
     anonymous, moderator, initiator = setup_users(project)
     member = member_factory(organisation=project.organisation)
     participant = user2

tests/offlineevents/rules/test_rules_view.py
@@ -1,6 +1,7 @@
 import pytest
 import rules
 
+from adhocracy4.projects.models import Access
 from tests.helpers import setup_users
 
 perm_name = 'a4_candy_offlineevents.view_offlineevent'
@@ -26,7 +27,7 @@ def test_rule(offline_event, user, member_factory):
 @pytest.mark.django_db
 def test_rule_project_private(offline_event_factory, user, user2,
                               member_factory):
-    offline_event = offline_event_factory(project__is_public=False)
+    offline_event = offline_event_factory(project__access=Access.PRIVATE)
     project = offline_event.project
     anonymous, moderator, initiator = setup_users(project)
     member = member_factory(organisation=project.organisation)

tests/organisations/test_organisation_models.py
@@ -6,6 +6,7 @@ from django.conf import settings
 from django.urls import reverse
 from freezegun import freeze_time
 
+from adhocracy4.projects.models import Access
 from apps.organisations import models
 from tests import helpers
 
@@ -143,8 +144,8 @@ def test_get_projects_list(module_factory, organisation, phase_factory,
                            project_factory, admin, user):
     pro1 = project_factory(organisation=organisation)
     pro2 = project_factory(organisation=organisation)
-    pro3 = project_factory(is_public=False, organisation=organisation)
-    pro4 = project_factory(is_public=False, organisation=organisation)
+    pro3 = project_factory(access=Access.PRIVATE, organisation=organisation)
+    pro4 = project_factory(access=Access.PRIVATE, organisation=organisation)
     pro5 = project_factory(organisation=organisation)
 
     module1 = module_factory(project=pro1)

tests/polls/rules/test_rules_comment_poll.py
@@ -1,6 +1,7 @@
 import pytest
 import rules
 
+from adhocracy4.projects.models import Access
 from apps.polls import phases
 from tests.helpers import freeze_phase
 from tests.helpers import freeze_post_phase
@@ -57,7 +58,8 @@ def test_phase_active_project_private(phase_factory, user, user2,
                                       member_factory, poll_factory):
     phase, _, project, item = setup_phase(phase_factory, poll_factory,
                                           phases.VotingPhase,
-                                          module__project__is_public=False)
+                                          module__project__access=Access.
+                                          PRIVATE)
     anonymous, moderator, initiator = setup_users(project)
     member = member_factory(organisation=project.organisation)
 

tests/questions/rules/test_rules_view.py
@@ -1,6 +1,7 @@
 import pytest
 import rules
 
+from adhocracy4.projects.models import Access
 from apps.questions import phases
 from tests.helpers import freeze_phase
 from tests.helpers import freeze_post_phase
@@ -48,7 +49,8 @@ def test_phase_active_project_private(phase_factory, question_factory,
                                       user, user2):
     phase, _, project, item = setup_phase(phase_factory, question_factory,
                                           phases.IssuePhase,
-                                          module__project__is_public=False)
+                                          module__project__access=Access.
+                                          PRIVATE)
     anonymous, moderator, initiator = setup_users(project)
     participant = user2
     project.participants.add(participant)

tests/topicprio/rules/test_rules_view.py
@@ -1,6 +1,7 @@
 import pytest
 import rules
 
+from adhocracy4.projects.models import Access
 from apps.topicprio import phases
 from tests.helpers import freeze_phase
 from tests.helpers import freeze_post_phase
@@ -52,7 +53,8 @@ def test_phase_active_project_private(phase_factory, topic_factory,
                                       user, user2, member_factory):
     phase, _, project, item = setup_phase(phase_factory, topic_factory,
                                           phases.PrioritizePhase,
-                                          module__project__is_public=False)
+                                          module__project__access=Access.
+                                          PRIVATE)
     anonymous, moderator, initiator = setup_users(project)
     member = member_factory(organisation=project.organisation)
     participant = user2

liqd/a4-kosmo

tests/debate/rules/test_rules_comment.py
@@ -1,6 +1,7 @@
 import pytest
 import rules
 
+from adhocracy4.projects.enums import Access
 from apps.debate import phases
 from tests.helpers import freeze_phase
 from tests.helpers import freeze_post_phase
@@ -52,7 +53,8 @@ def test_phase_active_project_private(phase_factory, user, user2,
                                       member_factory, subject_factory):
     phase, _, project, item = setup_phase(phase_factory, subject_factory,
                                           phases.DebatePhase,
-                                          module__project__is_public=False)
+                                          module__project__access=Access.
+                                          PRIVATE)
     anonymous, moderator, initiator = setup_users(project)
     member = member_factory(organisation=project.organisation)
 

tests/mapideas/rules/test_rules_comment.py
@@ -1,6 +1,7 @@
 import pytest
 import rules
 
+from adhocracy4.projects.enums import Access
 from apps.mapideas import phases
 from tests.helpers import freeze_phase
 from tests.helpers import freeze_post_phase
@@ -52,7 +53,8 @@ def test_phase_active_project_private(phase_factory, user, user2,
                                       member_factory, map_idea_factory):
     phase, _, project, item = setup_phase(phase_factory, map_idea_factory,
                                           phases.CollectPhase,
-                                          module__project__is_public=False)
+                                          module__project__access=Access.
+                                          PRIVATE)
     anonymous, moderator, initiator = setup_users(project)
     member = member_factory(organisation=project.organisation)
 

tests/mapideas/rules/test_rules_view.py
@@ -1,6 +1,7 @@
 import pytest
 import rules
 
+from adhocracy4.projects.enums import Access
 from apps.mapideas import phases
 from tests.helpers import freeze_phase
 from tests.helpers import freeze_post_phase
@@ -52,7 +53,8 @@ def test_phase_active_project_private(phase_factory, map_idea_factory,
                                       user, user2, member_factory):
     phase, _, project, item = setup_phase(phase_factory, map_idea_factory,
                                           phases.CollectPhase,
-                                          module__project__is_public=False)
+                                          module__project__access=Access.
+                                          PRIVATE)
     anonymous, moderator, initiator = setup_users(project)
     member = member_factory(organisation=project.organisation)
     participant = user2

tests/offlineevents/rules/test_rules_view.py
@@ -1,6 +1,7 @@
 import pytest
 import rules
 
+from adhocracy4.projects.enums import Access
 from tests.helpers import setup_users
 
 perm_name = 'a4_candy_offlineevents.view_offlineevent'
@@ -26,7 +27,7 @@ def test_rule(offline_event, user, member_factory):
 @pytest.mark.django_db
 def test_rule_project_private(offline_event_factory, user, user2,
                               member_factory):
-    offline_event = offline_event_factory(project__is_public=False)
+    offline_event = offline_event_factory(project__access=Access.PRIVATE)
     project = offline_event.project
     anonymous, moderator, initiator = setup_users(project)
     member = member_factory(organisation=project.organisation)

tests/organisations/test_organisation_models.py
@@ -6,6 +6,7 @@ from django.conf import settings
 from django.urls import reverse
 from freezegun import freeze_time
 
+from adhocracy4.projects.enums import Access
 from apps.organisations import models
 from tests import helpers
 
@@ -143,8 +144,8 @@ def test_get_projects_list(module_factory, organisation, phase_factory,
                            project_factory, admin, user):
     pro1 = project_factory(organisation=organisation)
     pro2 = project_factory(organisation=organisation)
-    pro3 = project_factory(is_public=False, organisation=organisation)
-    pro4 = project_factory(is_public=False, organisation=organisation)
+    pro3 = project_factory(access=Access.PRIVATE, organisation=organisation)
+    pro4 = project_factory(access=Access.PRIVATE, organisation=organisation)
     pro5 = project_factory(organisation=organisation)
 
     module1 = module_factory(project=pro1)

tests/polls/rules/test_rules_comment_poll.py
@@ -1,6 +1,7 @@
 import pytest
 import rules
 
+from adhocracy4.projects.enums import Access
 from apps.polls import phases
 from tests.helpers import freeze_phase
 from tests.helpers import freeze_post_phase
@@ -57,7 +58,8 @@ def test_phase_active_project_private(phase_factory, user, user2,
                                       member_factory, poll_factory):
     phase, _, project, item = setup_phase(phase_factory, poll_factory,
                                           phases.VotingPhase,
-                                          module__project__is_public=False)
+                                          module__project__access=Access.
+                                          PRIVATE)
     anonymous, moderator, initiator = setup_users(project)
     member = member_factory(organisation=project.organisation)
 

tests/questions/rules/test_rules_view.py
@@ -1,6 +1,7 @@
 import pytest
 import rules
 
+from adhocracy4.projects.enums import Access
 from apps.questions import phases
 from tests.helpers import freeze_phase
 from tests.helpers import freeze_post_phase
@@ -48,7 +49,8 @@ def test_phase_active_project_private(phase_factory, question_factory,
                                       user, user2):
     phase, _, project, item = setup_phase(phase_factory, question_factory,
                                           phases.IssuePhase,
-                                          module__project__is_public=False)
+                                          module__project__access=Access.
+                                          PRIVATE)
     anonymous, moderator, initiator = setup_users(project)
     participant = user2
     project.participants.add(participant)

tests/topicprio/rules/test_rules_view.py
@@ -1,6 +1,7 @@
 import pytest
 import rules
 
+from adhocracy4.projects.enums import Access
 from apps.topicprio import phases
 from tests.helpers import freeze_phase
 from tests.helpers import freeze_post_phase
@@ -52,7 +53,8 @@ def test_phase_active_project_private(phase_factory, topic_factory,
                                       user, user2, member_factory):
     phase, _, project, item = setup_phase(phase_factory, topic_factory,
                                           phases.PrioritizePhase,
-                                          module__project__is_public=False)
+                                          module__project__access=Access.
+                                          PRIVATE)
     anonymous, moderator, initiator = setup_users(project)
     member = member_factory(organisation=project.organisation)
     participant = user2