98a64eda5451dc03d4cacefc9d813af8409869ed
fuzzylogic2000 <k.lindenlaub@liqd.net>
2021-02-12 17:54:24 +0100

    WIP: apps/userdashboard: use api in moderator react component

---
+++ apps/userdashboard/assets/js/a4_candy_userdashboard/ModerationProjects.jsx
@@ -0,0 +1,23 @@
+/* global django */
+import React, { Component } from 'react'
+var ReactDOM = require('react-dom')
+
+class ModerationProjects extends React.Component {
+  render () {
+    return (
+      <div className="row mb-2">
+        <div className="col-12">
+          <h2>Projects</h2>
+          <span>project</span>
+        </div>
+      </div>
+    )
+  }
+}
+
+module.exports.renderModerationProjects = function (el) {
+  ReactDOM.render(
+    <ModerationProjects />,
+    el
+  )
+}
--- apps/userdashboard/assets/js/a4_candy_userdashboard/react_moderation_projects.jsx
+++ apps/userdashboard/assets/js/a4_candy_userdashboard/react_moderation_projects.jsx
@@ -1,22 +1,17 @@
 var React = require('react')
 var ReactDOM = require('react-dom')
 
-class ModerationProjects extends React.Component {
-  render () {
-    return (
-      <div className="row mb-2">
-        <div className="col-12">
-          <h2>Projects</h2>
-          <span>project</span>
-        </div>
-      </div>
-    )
-  }
-}
+import ModerationProjects from './ModerationProjects'
 
-module.exports.renderModerationProjects = function (el) {
-  ReactDOM.render(
-    <ModerationProjects />,
-    el
-  )
+function init () {
+  $('[data-aplus-widget="moderation_projects"').each(function(i, element) {
+    const moderationProjects = element.getAttribute('data-moderation-projects')
+    const projectApiUrl = element.getAttribute('data-project-api-url')
+    ReactDom.render(
+      <ModerationProjects
+        moderationProjects={moderationProjects}
+        projectApiUrl={projectApiUrl}
+      />,
+      element)
+  })
 }
