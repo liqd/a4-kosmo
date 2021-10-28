5c4f2309dda39a1385b8398d519da9fc09fc0bef
fuzzylogic2000 <k.lindenlaub@liqd.net>
2021-10-28 16:17:27 +0200

    django update: update deprecated methods

--- apps/classifications/serializers.py
+++ apps/classifications/serializers.py
@@ -1,4 +1,4 @@
-from django.utils.translation import ugettext_lazy as _
+from django.utils.translation import gettext_lazy as _
 from easy_thumbnails.files import get_thumbnailer
 from rest_framework import serializers
 
--- apps/userdashboard/urls.py
+++ apps/userdashboard/urls.py
@@ -1,21 +1,22 @@
-from django.conf.urls import url
+from django.urls import path
+from django.urls import re_path
 
 from . import views
 
 urlpatterns = [
-    url(r'^overview/$',
-        views.UserDashboardOverviewView.as_view(),
-        name='userdashboard-overview'),
-    url(r'^moderation/$',
-        views.UserDashboardModerationView.as_view(),
-        name='userdashboard-moderation'),
-    url(r'^overview/activities/$',
-        views.UserDashboardActivitiesView.as_view(),
-        name='userdashboard-activities'),
-    url(r'^overview/following/$',
-        views.UserDashboardFollowingView.as_view(),
-        name='userdashboard-following'),
-    url(r'^moderation/detail/(?P<slug>[-\w_]+)/$',
-        views.UserDashboardModerationDetailView.as_view(),
-        name='userdashboard-moderation-detail'),
+    path('overview/',
+         views.UserDashboardOverviewView.as_view(),
+         name='userdashboard-overview'),
+    path('moderation/',
+         views.UserDashboardModerationView.as_view(),
+         name='userdashboard-moderation'),
+    path('overview/activities/',
+         views.UserDashboardActivitiesView.as_view(),
+         name='userdashboard-activities'),
+    path('overview/following/',
+         views.UserDashboardFollowingView.as_view(),
+         name='userdashboard-following'),
+    re_path(r'^moderation/detail/(?P<slug>[-\w_]+)/$',
+            views.UserDashboardModerationDetailView.as_view(),
+            name='userdashboard-moderation-detail'),
 ]
