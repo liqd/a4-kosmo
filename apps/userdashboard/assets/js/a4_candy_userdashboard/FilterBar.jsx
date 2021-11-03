233947f3d80fa806c1b1e0da879fb1d2c9c48843
Kha <k.tran@liqd.net>
2021-11-03 17:49:36 +0000

    moderation/dashboard/filter: adding filter bar component and adjust logic to fetch by queryfilters

--- apps/userdashboard/assets/js/a4_candy_userdashboard/FilterBar.jsx
+++ apps/userdashboard/assets/js/a4_candy_userdashboard/FilterBar.jsx
@@ -1,29 +1,49 @@
-import React from 'react'
+import React, { useState } from 'react'
 import django from 'django'
 
-export const FilterBar = () => {
+const getItemByFilter = (items, filter) => {
+  return items.find(item => item.filter === filter)
+}
+
+export const FilterBar = (props) => {
+  const filterItems = [
+    { name: 'Pending', filter: '?is_pending=true' },
+    { name: 'Archived', filter: '?is_pending=false' },
+    { name: 'All', filter: '' }
+  ]
+  const [currFilterItem, setCurrFilterItem] =
+    useState(getItemByFilter(filterItems, props.selectedFilter))
+
+  const onSelectFilter = (filterItem) => {
+    setCurrFilterItem(filterItem)
+    props.onFilterChange(filterItem.filter)
+  }
+
   return (
-    <div className="filter-bar justify-content-end" role="group" aria-label={django.gettext('Filter')}>
-      <div className="me-sm-1 mt-2 mt-sm-0">
+    <div
+      className="filter-bar justify-content-end"
+      role="group"
+      aria-label={django.gettext('Filter')}
+    >
+      <div className="mt-2 mt-sm-0">
         <div className="dropdown dropdown-menu-end">
           <button
             type="button"
-            className="dropdown-toggle btn btn--light btn--select show"
+            className="dropdown-toggle btn btn--light btn--select"
             data-bs-toggle="dropdown"
+            aria-expanded="false"
           >
-            {django.gettext('Filter')}
+            {django.gettext('Filter')}: {currFilterItem.name}
             <i className="fa fa-caret-down" aria-hidden="true" />
           </button>
           <ul className="dropdown-menu">
-            <li>
-              <a className="selected" href="/#">all</a>
-            </li>
-            <li>
-              <a href="/#">pending</a>
-            </li>
-            <li>
-              <a href="/#">archived</a>
-            </li>
+            {filterItems.map((filterItem, i) => (
+              <li key={`${i}_${filterItem.name}`}>
+                <button onClick={() => onSelectFilter(filterItem)}>
+                  {django.gettext(filterItem.name)}
+                </button>
+              </li>
+            ))}
           </ul>
         </div>
       </div>
--- apps/userdashboard/assets/js/a4_candy_userdashboard/ModerationNotificationList.jsx
+++ apps/userdashboard/assets/js/a4_candy_userdashboard/ModerationNotificationList.jsx
@@ -9,37 +9,38 @@ export default class ModerationNotificationList extends Component {
     super(props)
 
     this.state = {
-      aiClassifications: [],
-      userClassifications: [],
+      notifications: [],
+      filter: '',
       isLoaded: false
     }
   }
 
   componentDidMount () {
-    this.loadData()
-    this.timer = setInterval(() => this.loadData(), 3000)
+    this.loadData(this.state.filter)
+    this.timer = setInterval(() => this.loadData(this.state.filter), 3000)
   }
 
-  async loadData () {
-    fetch(this.props.aiclassificationApiUrl)
-      .then(res => res.json())
-      .then(json => {
-        this.setState({
-          aiClassifications: json
-        })
-      }).catch((err) => {
-        console.log(err)
-      })
-    fetch(this.props.userclassificationApiUrl)
-      .then(res => res.json())
-      .then(json => {
-        this.setState({
-          userClassifications: json,
-          isLoaded: true
-        })
-      }).catch((err) => {
-        console.log(err)
-      })
+  filterChangeHandle (filter) {
+    this.setState({ filter: filter })
+    this.setState({ isLoaded: false })
+    this.loadData(filter)
+  }
+
+  async loadData (filter = undefined) {
+    const aiUrl = filter
+      ? this.props.aiclassificationApiUrl + filter
+      : this.props.aiclassificationApiUrl
+    const aiFetch = await fetch(aiUrl)
+    const aiNotifications = await aiFetch.json()
+    aiNotifications && aiNotifications.forEach(nf => { nf.meta = { aiClassified: true } })
+
+    const userUrl = filter
+      ? this.props.userclassificationApiUrl + filter
+      : this.props.userclassificationApiUrl
+    const userFetch = await fetch(userUrl)
+    const userNotifications = await userFetch.json()
+
+    this.setState({ notifications: [...aiNotifications, ...userNotifications], isLoaded: true })
   }
 
   componentWillUnmount () {
@@ -48,56 +49,55 @@ export default class ModerationNotificationList extends Component {
   }
 
   render () {
-    const { isLoaded, aiClassifications, userClassifications } = this.state
+    const { isLoaded, notifications } = this.state
     const { projectTitle, organisation, projectUrl } = this.props
     const byText = django.pgettext('kosmo', 'By ')
     const loadingText = django.pgettext('kosmo', 'Loading...')
 
-    if (!isLoaded) {
-      return <div>{loadingText}</div>
-    }
-
     return (
       <div className="row mb-2">
         <div className="col-12">
-          <h1 className="m-0"><a href={projectUrl}>{projectTitle}</a></h1>
-          <span className="text-muted">{byText}{organisation}</span>
+          <h1 className="m-0">
+            <a href={projectUrl}>{projectTitle}</a>
+          </h1>
+          <span className="text-muted">
+            {byText}
+            {organisation}
+          </span>
           <div className="mt-3">
-            <FilterBar />
+            <FilterBar
+              onFilterChange={(filter) => this.filterChangeHandle(filter)}
+              selectedFilter={this.state.filter}
+            />
           </div>
-          <ul className="ps-0 mt-5">
-            {userClassifications.map((item, i) => (
-              <li className="list-item" key={i}>
-                <ModerationNotification
-                  apiUrl={item.api_url}
-                  classification={item.classification}
-                  commentText={item.comment_text}
-                  commentUrl={item.comment.comment_url}
-                  created={item.created}
-                  isPending={item.is_pending}
-                  userImage={item.comment.user_image}
-                  userName={item.comment.user_name}
-                  userProfileUrl={item.comment.user_profile_url}
-                />
-              </li>
-            ))}
-            {aiClassifications.map((item, i) => (
-              <li className="list-item" key={i}>
-                <ModerationNotification
-                  apiUrl={item.api_url}
-                  classification={item.classification}
-                  commentText={item.comment_text}
-                  commentUrl={item.comment.comment_url}
-                  created={item.created}
-                  isPending={item.is_pending}
-                  userImage={item.comment.user_image}
-                  userName={item.comment.user_name}
-                  userProfileUrl={item.comment.user_profile_url}
-                  aiClassified
-                />
-              </li>
-            ))}
-          </ul>
+          {!isLoaded
+            ? (
+              <div className="d-flex justify-content-center">
+                <div className="spinner-border" role="status">
+                  <span className="sr-only">{loadingText}</span>
+                </div>
+              </div>
+              )
+            : (
+              <ul className="ps-0 mt-5">
+                {notifications.map((item, i) => (
+                  <li className="list-item" key={i}>
+                    <ModerationNotification
+                      apiUrl={item.api_url}
+                      classification={item.classification}
+                      commentText={item.comment_text}
+                      commentUrl={item.comment.comment_url}
+                      created={item.created}
+                      isPending={item.is_pending}
+                      userImage={item.comment.user_image}
+                      userName={item.comment.user_name}
+                      userProfileUrl={item.comment.user_profile_url}
+                      aiClassified={item?.meta?.aiClassified}
+                    />
+                  </li>
+                ))}
+              </ul>
+              )}
         </div>
       </div>
     )
