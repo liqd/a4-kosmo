b59b6bd249c6021d9abaa36f8d23407f6fc047a5
fuzzylogic2000 <k.lindenlaub@liqd.net>
2021-11-15 12:07:05 +0100

    settings: change for kosmo fork

--- adhocracy-plus/config/settings/test.py
+++ adhocracy-plus/config/settings/test.py
@@ -10,3 +10,5 @@ SITE_ID=1
 CAPTCHA_TEST_ACCEPTED_ANSWER = 'testpass'
 CAPTCHA_URL = 'https://captcheck.netsyms.com/api.php'
 WAGTAILADMIN_BASE_URL = 'http://localhost:8004'
+
+AI_API_AUTH_TOKEN = ''
