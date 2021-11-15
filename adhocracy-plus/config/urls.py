8d69574017af4c267a3501a8218d5a9027d44c13
fuzzylogic2000 <k.lindenlaub@liqd.net>
2021-11-15 14:27:30 +0100

    apps/moderation dashboard: add settings

--- adhocracy-plus/config/urls.py
+++ adhocracy-plus/config/urls.py
@@ -24,6 +24,8 @@ from adhocracy4.polls.api import PollViewSet
 from adhocracy4.ratings.api import RatingViewSet
 from adhocracy4.reports.api import ReportViewSet
 from apps.account.api import AccountViewSet
+from apps.classifications.api import AIClassificationViewSet
+from apps.classifications.api import UserClassificationViewSet
 from apps.contrib import views as contrib_views
 from apps.contrib.sitemaps import static_sitemap_index
 from apps.documents.api import DocumentViewSet
@@ -35,6 +37,9 @@ from apps.moderatorremark.api import ModeratorRemarkViewSet
 from apps.organisations.sitemaps import organisations_sitemap_index
 from apps.projects.api import AppModuleViewSet
 from apps.projects.api import AppProjectsViewSet
+from apps.projects.api import ModerationProjectsViewSet
+from apps.reports.api import ReportViewSet
+from apps.userdashboard.routers import ModerationDetailDefaultRouter
 from apps.users.api import UserViewSet
 from apps.users.decorators import user_is_project_admin
 
@@ -46,6 +51,8 @@ router.register(r"app-projects", AppProjectsViewSet, basename="app-projects")
 router.register(r"app-modules", AppModuleViewSet, basename="app-modules")
 router.register(r"users", UserViewSet, basename="users")
 
+router.register(r'moderationprojects', ModerationProjectsViewSet,
+                basename='moderationprojects')
 
 module_router = a4routers.ModuleDefaultRouter()
 # FIXME: rename to 'chapters'
@@ -59,6 +66,12 @@ module_router.register(r"ideas", IdeaViewSet, basename="ideas")
 
 likes_router = LikesDefaultRouter()
 likes_router.register(r"likes", LikesViewSet, basename="likes")
+
+moderation_router = ModerationDetailDefaultRouter()
+moderation_router.register(r'userclassifications', UserClassificationViewSet,
+                           basename='userclassifications')
+moderation_router.register(r'aiclassifications', AIClassificationViewSet,
+                           basename='aiclassifications')
 
 orga_router = a4routers.OrganisationDefaultRouter()
 
@@ -79,12 +92,14 @@ urlpatterns = [
     re_path(r"^account/", include("apps.account.urls")),
     re_path(r"^embed/", include("apps.embed.urls")),
     re_path(r"^profile/", include("apps.users.urls")),
+    re_path(r'^userdashboard/', include('apps.userdashboard.urls')),
     re_path(r"^i18n/", include(i18n)),
     # API urls
     re_path(r"^api/", include(ct_router.urls)),
     re_path(r"^api/", include(module_router.urls)),
     re_path(r"^api/", include(orga_router.urls)),
     re_path(r"^api/", include(likes_router.urls)),
+    re_path(r'^api/', include(moderation_router.urls)),
     re_path(r"^api/", include(router.urls)),
     re_path(r"^api/login", obtain_auth_token, name="api-login"),
     re_path(r"^api/account/", AccountViewSet.as_view(), name="api-account"),
