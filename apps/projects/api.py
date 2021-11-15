105a05e09285fccb69803138862981de444ffc1d
fuzzylogic2000 <k.lindenlaub@liqd.net>
2021-11-15 13:55:24 +0100

    apps/projects: add project api for moderation projects

--- apps/projects/api.py
+++ apps/projects/api.py
@@ -3,7 +3,9 @@ from rest_framework import viewsets
 
 from adhocracy4.modules.models import Module
 from adhocracy4.projects.models import Project
+from apps.projects.serializers import ProjectSerializer
 
+from . import helpers
 from .serializers import AppModuleSerializer
 from .serializers import AppProjectSerializer
 
@@ -30,3 +32,16 @@ class AppModuleViewSet(viewsets.ReadOnlyModelViewSet):
             is_draft=False,
             project__is_app_accessible=True
         )
+
+
+class ModerationProjectsViewSet(viewsets.ReadOnlyModelViewSet):
+    serializer_class = ProjectSerializer
+    permission_classes = (permissions.IsAuthenticated,)
+
+    def get_queryset(self):
+        project_offensive_count = \
+            {project: helpers.get_num_classifications(project)
+             for project in list(self.request.user.project_moderator.all())}
+        return sorted(project_offensive_count,
+                      key=project_offensive_count.get,
+                      reverse=True)
