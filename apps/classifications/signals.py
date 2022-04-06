e4b20b932806aa8d51e0202d214e7abfc284bd91
rine <k.schmid@liqd.net>
2022-04-06 14:47:02 +0200

    classifications/signals//settings: use version 3 of kosmo api and process new categories

--- apps/classifications/signals.py
+++ apps/classifications/signals.py
@@ -19,15 +19,37 @@ def get_ai_classification(sender, instance, **kwargs):
     if hasattr(settings, 'AI_USAGE') and settings.AI_USAGE:
         if hasattr(settings, 'AI_API_AUTH_TOKEN') and \
                 settings.AI_API_AUTH_TOKEN:
+            if hasattr(settings, 'AI_API_VERSION') and \
+                    settings.AI_API_VERSION:
+                ai_api_version = settings.AI_API_VERSION
+            else:
+                ai_api_version = '2.0'
             try:
-                response = call_ai_api(comment=instance)
-                if response.status_code == 200 \
-                        and response.json()['classification'] == 'OFFENSE':
+                response = call_ai_api(comment=instance,
+                                       version=ai_api_version)
+                if response.status_code == 200:
+                    # version 2
+                    if ai_api_version == '2.0' \
+                            and response.json()['classification'] == 'OFFENSE':
 
-                    classification = AIClassification(
-                        comment=instance,
-                        classification='OFFENSIVE')
-                    classification.save()
+                        classification = AIClassification(
+                            comment=instance,
+                            classifications=['OFFENSIVE']
+                        )
+                        classification.save()
+                    # version 3
+                    elif ai_api_version == '3.0' and \
+                            1 in response.json()['classification'].values():
+                        classifications_dict = \
+                            response.json()['classification']
+                        classifications = [c for c in classifications_dict
+                                           if classifications_dict[c] == 1]
+                        classification = AIClassification(
+                            comment=instance,
+                            classifications=classifications
+                        )
+                        classification.save()
+
             except httpx.HTTPError as e:
                 logger.error('Error connecting to %s: %s',
                              settings.AI_API_URL, str(e))
@@ -47,12 +69,16 @@ def skip_retry(e):
                       max_tries=4,
                       factor=2,
                       giveup=skip_retry)
-def call_ai_api(comment):
+def call_ai_api(comment, version):
     response = client.post(settings.AI_API_URL,
                            data={'comment': comment.comment},
                            headers={'Authorization': 'Token {}'.format(
                                     settings.AI_API_AUTH_TOKEN),
-                                    'Accept': 'application/json; version=2.0'})
+                                    'Accept': 'application/json; '
+                                              'version={}'.format(
+                                    version)
+                                    }
+                           )
     response.raise_for_status()
 
     return response
