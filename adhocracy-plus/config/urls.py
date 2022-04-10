60d50cbab3f41e42162410af2bdfd63f9dbffcc7
rine <k.schmid@liqd.net>
2022-04-10 13:28:32 +0200

    apps/contrib//config: add CommentDefaultRouter and use in route for moderatorstatement

--- adhocracy-plus/config/urls.py
+++ adhocracy-plus/config/urls.py
@@ -27,12 +27,14 @@ from apps.account.api import AccountViewSet
 from apps.classifications.api import AIClassificationViewSet
 from apps.classifications.api import UserClassificationViewSet
 from apps.contrib import views as contrib_views
+from apps.contrib.routers import CommentDefaultRouter
 from apps.contrib.sitemaps import static_sitemap_index
 from apps.documents.api import DocumentViewSet
 from apps.ideas.api import IdeaViewSet
 from apps.interactiveevents.api import LikesViewSet
 from apps.interactiveevents.api import LiveQuestionViewSet
 from apps.interactiveevents.routers import LikesDefaultRouter
+from apps.moderatorfeedback.api import ModeratorCommentStatementViewSet
 from apps.moderatorremark.api import ModeratorRemarkViewSet
 from apps.organisations.sitemaps import organisations_sitemap_index
 from apps.projects.api import AppModuleViewSet
@@ -83,6 +85,11 @@ ct_router.register(r'moderatorremarks', ModeratorRemarkViewSet,
 ct_router.register(r'comment-moderate', CommentModerateSet,
                    basename='comment-moderate')
 
+comment_router = CommentDefaultRouter()
+comment_router.register(r'moderatorstatement',
+                        ModeratorCommentStatementViewSet,
+                        basename='moderatorstatement')
+
 urlpatterns = [
     # General platform urls
     re_path(r'^django-admin/', admin.site.urls),
@@ -102,6 +109,7 @@ urlpatterns = [
     re_path(r'^api/', include(orga_router.urls)),
     re_path(r'^api/', include(likes_router.urls)),
     re_path(r'^api/', include(moderation_router.urls)),
+    re_path(r'^api/', include(comment_router.urls)),
     re_path(r'^api/', include(router.urls)),
     re_path(r'^api/login', obtain_auth_token, name='api-login'),
     re_path(r'^api/account/', AccountViewSet.as_view(), name='api-account'),
