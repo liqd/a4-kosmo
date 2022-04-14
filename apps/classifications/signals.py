2bf83663ba54368505885d340157cb9a43b6923c
rine <k.schmid@liqd.net>
2022-04-14 08:42:20 +0200

    classifications/signals: only send comment to ai if created or text changed

--- apps/classifications/signals.py
+++ apps/classifications/signals.py
@@ -15,47 +15,49 @@ logger = logging.getLogger(__name__)
 
 
 @receiver(signals.post_save, sender=Comment)
-def get_ai_classification(sender, instance, **kwargs):
-    if hasattr(settings, 'AI_USAGE') and settings.AI_USAGE:
-        if hasattr(settings, 'AI_API_AUTH_TOKEN') and \
-                settings.AI_API_AUTH_TOKEN:
-            if hasattr(settings, 'AI_API_VERSION') and \
-                    settings.AI_API_VERSION:
-                ai_api_version = settings.AI_API_VERSION
+def get_ai_classification(sender, instance, created, update_fields, **kwargs):
+    if created or (update_fields and 'comment' in update_fields):
+        if hasattr(settings, 'AI_USAGE') and settings.AI_USAGE:
+            if hasattr(settings, 'AI_API_AUTH_TOKEN') and \
+                    settings.AI_API_AUTH_TOKEN:
+                if hasattr(settings, 'AI_API_VERSION') and \
+                        settings.AI_API_VERSION:
+                    ai_api_version = settings.AI_API_VERSION
+                else:
+                    ai_api_version = '2.0'
+                try:
+                    response = call_ai_api(comment=instance,
+                                           version=ai_api_version)
+                    if response.status_code == 200:
+                        # version 2
+                        if ai_api_version == '2.0' \
+                                and response.json()['classification']\
+                                == 'OFFENSE':
+
+                            classification = AIClassification(
+                                comment=instance,
+                                classifications=['OFFENSIVE']
+                            )
+                            classification.save()
+                        # version 3
+                        elif ai_api_version == '3.0' and 1 in \
+                                response.json()['classification'].values():
+                            classifications_dict = \
+                                response.json()['classification']
+                            classifications = [c for c in classifications_dict
+                                               if classifications_dict[c] == 1]
+                            classification = AIClassification(
+                                comment=instance,
+                                classifications=classifications
+                            )
+                            classification.save()
+
+                except httpx.HTTPError as e:
+                    logger.error('Error connecting to %s: %s',
+                                 settings.AI_API_URL, str(e))
             else:
-                ai_api_version = '2.0'
-            try:
-                response = call_ai_api(comment=instance,
-                                       version=ai_api_version)
-                if response.status_code == 200:
-                    # version 2
-                    if ai_api_version == '2.0' \
-                            and response.json()['classification'] == 'OFFENSE':
-
-                        classification = AIClassification(
-                            comment=instance,
-                            classifications=['OFFENSIVE']
-                        )
-                        classification.save()
-                    # version 3
-                    elif ai_api_version == '3.0' and \
-                            1 in response.json()['classification'].values():
-                        classifications_dict = \
-                            response.json()['classification']
-                        classifications = [c for c in classifications_dict
-                                           if classifications_dict[c] == 1]
-                        classification = AIClassification(
-                            comment=instance,
-                            classifications=classifications
-                        )
-                        classification.save()
-
-            except httpx.HTTPError as e:
-                logger.error('Error connecting to %s: %s',
-                             settings.AI_API_URL, str(e))
-        else:
-            logger.error('No ai api auth token provided. '
-                         'Disable ai usage or provide token.')
+                logger.error('No ai api auth token provided. '
+                             'Disable ai usage or provide token.')
 
 
 def skip_retry(e):
