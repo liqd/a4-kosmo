88f9d29857a3dcfffcb871961070b26d841c0128
rine <k.schmid@liqd.net>
2022-04-19 12:08:39 +0200

    moderatorfeedback//config: overwrite CommentSerializer and ThreadSerializer to add statement and use in api

--- apps/moderatorfeedback/api.py
+++ apps/moderatorfeedback/api.py
@@ -2,10 +2,13 @@ from rest_framework import mixins
 from rest_framework import viewsets
 
 from adhocracy4.api.permissions import ViewSetRulesPermission
+from adhocracy4.comments_async import api as a4_api
 from apps.contrib.api import CommentMixin
 from apps.moderatorfeedback.models import ModeratorCommentStatement
 from apps.moderatorfeedback.serializers import \
     ModeratorCommentStatementSerializer
+from apps.moderatorfeedback.serializers import ThreadListSerializer
+from apps.moderatorfeedback.serializers import ThreadSerializer
 
 
 class ModeratorCommentStatementViewSet(CommentMixin,
@@ -23,3 +26,11 @@ class ModeratorCommentStatementViewSet(CommentMixin,
     def get_queryset(self):
         return ModeratorCommentStatement.objects.filter(
             comment=self.comment)
+
+
+class CommentWithStatementViewSet(a4_api.CommentViewSet):
+
+    def get_serializer_class(self):
+        if self.action == 'list':
+            return ThreadListSerializer
+        return ThreadSerializer
