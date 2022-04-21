9b434320f50311bef102b324de3cd8ffbd2a6273
Kha <k.tran@liqd.net>
2022-04-21 17:22:01 +0100

    a4/Alert: changing imports to use from a4
    apps/contrib/Alert.jsx: removing Alert component from Kosmo
    
    Revert "apps/contrib/Alert.jsx: removing Alert component from Kosmo"
    
    This reverts commit ca03c733b23d9efc009f70c2847b265273729cb2.
    
    Revert import of Alert (will come with Aplus)
    
    squash

--- apps/userdashboard/assets/js/a4_candy_userdashboard/ModerationStatementForm.jsx
+++ apps/userdashboard/assets/js/a4_candy_userdashboard/ModerationStatementForm.jsx
@@ -1,8 +1,6 @@
 import React, { useState } from 'react'
 import django from 'django'
 
-import Alert from '../../../../contrib/assets/Alert'
-
 export const ModerationStatementForm = (props) => {
   const [statement, setStatement] =
     useState((props.editing && props.initialStatement.statement) || '')
@@ -10,7 +8,6 @@ export const ModerationStatementForm = (props) => {
   const translated = {
     placeholder: django.pgettext('kosmo', 'Write statement'),
     charCount: django.pgettext('kosmo', ' characters'),
-    alertError: django.gettext('kosmo', 'Your statement could not be saved, please check if there is already one saved.'),
     submitLabel: django.pgettext('kosmo', 'submit statement'),
     submitEditLabel: django.pgettext('kosmo', 'update statement')
   }
@@ -24,15 +21,8 @@ export const ModerationStatementForm = (props) => {
     }
   }
 
-  const removeAlert = (e) => {
-    this.setState({
-      alert: null
-    })
-  }
-
   return (
     <form className="general-form" onSubmit={handleSubmit}>
-      <Alert onClick={() => removeAlert()}>{translated.alertError}</Alert>
       <textarea
         className="a4-comments__textarea--small form-group"
         placeholder={translated.placeholder}
