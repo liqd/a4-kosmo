d49b9c2a057f7476867164f87696a22ee151b9e5
phillimorland <5871230+phillimorland@users.noreply.github.com>
2022-04-21 16:51:31 +0200

    ModerationNotification: update report text and add count and last edited, rm old strings - not working

--- apps/userdashboard/assets/js/a4_candy_userdashboard/ModerationNotification.jsx
+++ apps/userdashboard/assets/js/a4_candy_userdashboard/ModerationNotification.jsx
@@ -194,6 +194,13 @@ export default class ModerationNotification extends Component {
     })
   }
 
+  translatedReportText (reportsFound) {
+    const tmp = django.ngettext('kosmo', '\'s {}comment{} has been reported 1 time since it\'s creation', '\'s {}comment{} has been reported %s times since it\'s creation', reportsFound)
+    return (
+      django.interpolate(tmp, [reportsFound])
+    )
+  }
+
   componentDidMount () {
     const moderationStatementApiUrl =
       `/api/comments/${this.props.commentPk}/moderatorstatement/`
@@ -211,13 +218,7 @@ export default class ModerationNotification extends Component {
   }
 
   render () {
-    const { classifications, commentText, commentUrl, created, userImage, userName, userProfileUrl, aiClassified } = this.props
-    const offensiveTextReport = django.pgettext('kosmo', ' posted a {}comment{} that has been reported as %(classification)s')
-    const offensiveTextAI = django.pgettext('kosmo', ' posted a {}comment{} that might be %(classification)s')
-    /* eslint-disable */
-    const offensiveTextReportInterpolated = django.interpolate(offensiveTextReport, { 'classification': classifications }, true)
-    const offensiveTextAIInterpolated = django.interpolate(offensiveTextAI, { 'classification': classifications }, true)
-    /* eslint-enable */
+    const { classifications, commentText, commentUrl, created, modified, userImage, userName, userProfileUrl, aiClassified } = this.props
     const classificationText = django.pgettext('kosmo', 'Classification: ')
     const aiText = django.pgettext('kosmo', 'AI')
     const blockText = django.pgettext('kosmo', ' Block')
@@ -235,6 +236,13 @@ export default class ModerationNotification extends Component {
       userImageDiv = <div className="user-avatar user-avatar--small user-avatar--shadow mb-1 userindicator__btn-img" style={sectionStyle} />
     }
 
+    let commentChangeLog
+    if (modified === null) {
+      commentChangeLog = django.pgettext('kosmo', 'Created on ' + created)
+    } else {
+      commentChangeLog = django.pgettext('kosmo', 'Last edited on ' + created)
+    }
+
     return (
       <>
         <li className="list-item">
@@ -246,9 +254,9 @@ export default class ModerationNotification extends Component {
               <div className="col-sm-7 col-md-8">
                 <div><i className="fas fa-exclamation-circle me-1" aria-hidden="true" />
                   {userProfileUrl ? <a href={userProfileUrl}>{userName}</a> : userName}
-                  {aiClassified ? this.getLink(offensiveTextAIInterpolated, commentUrl) : this.getLink(offensiveTextReportInterpolated, commentUrl)}
+                  {this.getLink(this.translatedReportText(1), commentUrl)}
                 </div>
-                <div>{created}</div>
+                <div>{commentChangeLog}</div>
               </div>
               {this.state.isPending &&
                 <div className="col">
