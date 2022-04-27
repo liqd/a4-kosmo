f2f83acd9294b802f8e5958948507451de146de6
rine <k.schmid@liqd.net>
2022-04-27 10:29:09 +0200

    userdashboard/assets: use statement_api_url and fix some issues

--- apps/userdashboard/assets/js/a4_candy_userdashboard/ModerationStatement.jsx
+++ apps/userdashboard/assets/js/a4_candy_userdashboard/ModerationStatement.jsx
@@ -7,12 +7,7 @@ export const ModerationStatement = (props) => {
     delete: django.pgettext('kosmo', 'delete'),
     edit: django.pgettext('kosmo', 'edit'),
     statementTitle: django.pgettext('kosmo', 'Moderator\'s statement'),
-    editWasOn: django.pgettext('kosmo', 'Last edit was on')
-  }
-
-  const formatDate = (date) => {
-    const editDate = new Date(date)
-    return editDate.toLocaleDateString()
+    editWasOn: django.pgettext('kosmo', 'Last edit was on ')
   }
 
   return (
@@ -21,41 +16,42 @@ export const ModerationStatement = (props) => {
         <div className="col-7 col-md-8 userdashboard-mod-statement__header">
           <div>{translated.statementTitle}</div>
         </div>
-        <div className="col">
-          <div className="text-end">
-            <button
-              type="button" className="dropdown-toggle btn btn--none" aria-haspopup="true"
-              aria-expanded="false" data-bs-toggle="dropdown"
-            >
-              <i className="fas fa-ellipsis-v" aria-hidden="true" />
-            </button>
-            <ul className="dropdown-menu dropdown-menu-end">
-              <li key="1">
-                <button
-                  className="dropdown-item"
-                  type="button"
-                  onClick={() => props.onDelete(pk)}
-                >
-                  {translated.delete}
-                </button>
-              </li>
-              <li key="2">
-                <button
-                  className="dropdown-item"
-                  type="button"
-                  onClick={props.onEdit}
-                >
-                  {translated.edit}
-                </button>
-              </li>
-            </ul>
-          </div>
-        </div>
+        {props.notificationIsPending &&
+          <div className="col">
+            <div className="text-end">
+              <button
+                type="button" className="dropdown-toggle btn btn--none" aria-haspopup="true"
+                aria-expanded="false" data-bs-toggle="dropdown"
+              >
+                <i className="fas fa-ellipsis-v" aria-hidden="true" />
+              </button>
+              <ul className="dropdown-menu dropdown-menu-end">
+                <li key="1">
+                  <button
+                    className="dropdown-item"
+                    type="button"
+                    onClick={() => props.onDelete(pk)}
+                  >
+                    {translated.delete}
+                  </button>
+                </li>
+                <li key="2">
+                  <button
+                    className="dropdown-item"
+                    type="button"
+                    onClick={props.onEdit}
+                  >
+                    {translated.edit}
+                  </button>
+                </li>
+              </ul>
+            </div>
+          </div>}
       </div>
       <div className="row">
         <div className="col">
           <p>
-            {`${translated.editWasOn} ${formatDate(lastEdit)}`}
+            {translated.editWasOn} {lastEdit}
           </p>
         </div>
       </div>
