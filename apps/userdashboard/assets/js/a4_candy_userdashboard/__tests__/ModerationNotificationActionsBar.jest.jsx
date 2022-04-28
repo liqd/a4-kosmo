e6e678cbb2f3d8c0430840420147cd76528577c9
Kha <k.tran@liqd.net>
2022-04-28 15:31:08 +0100

    jstests/ModerationNotificationActionsBar.jsx: component test
    squash
    
    squash

---
+++ apps/userdashboard/assets/js/a4_candy_userdashboard/__tests__/ModerationNotificationActionsBar.jest.jsx
@@ -0,0 +1,112 @@
+import React from 'react'
+import { render, fireEvent } from '@testing-library/react'
+import '@testing-library/jest-dom'
+import { ModerationNotificationActionsBar } from '../ModerationNotificationActionsBar'
+
+test('Pending has three buttons', () => {
+  const mockPending = true
+  const tree = render(
+    <ModerationNotificationActionsBar
+      isPending={mockPending}
+    />
+  )
+  const buttons = tree.container.querySelectorAll('button')
+  expect(buttons.length).toBe(3)
+})
+
+test('Pending with reply button disabled', () => {
+  const mockPending = true
+  const mockDisabled = true
+  const tree = render(
+    <ModerationNotificationActionsBar
+      isPending={mockPending}
+      isDisabled={mockDisabled}
+    />
+  )
+  const button =
+    tree.container.querySelector('#moderation-notification-actions-bar-button-reply')
+  expect(button).toBeDisabled()
+})
+
+test('Pending with highlight button disabled', () => {
+  const mockPending = true
+  const mockBlocked = true
+  const mockHighlighted = false
+  const tree = render(
+    <ModerationNotificationActionsBar
+      isPending={mockPending}
+      isBlocked={mockBlocked}
+      isHighlighted={mockHighlighted}
+    />
+  )
+  const button =
+    tree.container.querySelector('#moderation-notification-actions-bar-button-highlight')
+  expect(button).toBeDisabled()
+})
+
+test('Pending is highlighted', () => {
+  const mockPending = true
+  const mockHighlighted = true
+  const tree = render(
+    <ModerationNotificationActionsBar
+      isPending={mockPending}
+      isHighlighted={mockHighlighted}
+    />
+  )
+  const buttons = tree.container.querySelectorAll('button')
+  expect(buttons.length).toBe(3)
+})
+
+test('Pending clicks: reply --> highlight --> block', () => {
+  const mockPending = true
+  const mockDisabled = false
+  const mockBlocked = false
+  const mockHighlighted = false
+  const mockToggleFn = jest.fn()
+  const tree = render(
+    <ModerationNotificationActionsBar
+      isPending={mockPending}
+      isDisabled={mockDisabled}
+      isBlocked={mockBlocked}
+      isHighlighted={mockHighlighted}
+      onToggleForm={mockToggleFn}
+      onToggleBlock={mockToggleFn}
+      onToggleHighlight={mockToggleFn}
+    />
+  )
+  const replyButton =
+    tree.container.querySelector('#moderation-notification-actions-bar-button-reply')
+  const highlightButton =
+    tree.container.querySelector('#moderation-notification-actions-bar-button-highlight')
+  const blockButton =
+    tree.container.querySelector('#moderation-notification-actions-bar-button-block')
+
+  fireEvent.click(replyButton)
+  fireEvent.click(highlightButton)
+  fireEvent.click(blockButton)
+  expect(mockToggleFn).toHaveBeenCalledTimes(3)
+})
+
+test('Archived has one button', () => {
+  const mockPending = false
+  const tree = render(
+    <ModerationNotificationActionsBar
+      isPending={mockPending}
+    />
+  )
+  const buttons = tree.container.querySelectorAll('button')
+  expect(buttons.length).toBe(1)
+})
+
+test('Archived blocked shows blocked text', () => {
+  const mockPending = false
+  const mockBlocked = true
+  const tree = render(
+    <ModerationNotificationActionsBar
+      isPending={mockPending}
+      isBlocked={mockBlocked}
+    />
+  )
+  const blockedTextDiv = tree.container.querySelector('.fw-bold')
+  expect(blockedTextDiv).toBeTruthy()
+})
