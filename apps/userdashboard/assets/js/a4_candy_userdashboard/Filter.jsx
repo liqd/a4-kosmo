9ceb40aaec76cecc1e22f8bf273099411d066d51
rine <k.schmid@liqd.net>
2022-05-06 17:11:19 +0200

    usserdashboard/assets/Filter//ModerationNotificationList: add classifications filter

--- apps/userdashboard/assets/js/a4_candy_userdashboard/Filter.jsx
+++ apps/userdashboard/assets/js/a4_candy_userdashboard/Filter.jsx
@@ -1,46 +1,40 @@
 import React, { useState } from 'react'
 import django from 'django'
 
-const getItemByFilter = (items, filter) => {
-  return items.find(item => item.filter === filter)
+const getItemByValue = (items, value) => {
+  return items.find(item => item.value === value)
 }
 
 export const Filter = (props) => {
-  console.log(props)
   const [currFilterItem, setCurrFilterItem] =
-    useState(getItemByFilter(props.filterItems, props.selectedFilter))
+    useState(getItemByValue(props.filterItems, props.selectedFilter))
 
   const onSelectFilter = (filterItem) => {
     setCurrFilterItem(filterItem)
-    props.onFilterChange(filterItem)
+    props.onFilterChange(filterItem.value)
   }
 
   return (
-    <div
-      className="filter-bar justify-content-end"
-      aria-label={django.gettext('Filter')}
-    >
-      <div className="mt-2 mt-sm-0">
-        <div className="dropdown dropdown-menu-end">
-          <button
-            type="button"
-            className="dropdown-toggle btn btn--light btn--select"
-            data-bs-toggle="dropdown"
-            aria-expanded="false"
-          >
-            {django.gettext('Filter')}: {currFilterItem.name}
-            <i className="fa fa-caret-down" aria-hidden="true" />
-          </button>
-          <ul className="dropdown-menu">
-            {props.filterItems.map((filterItem, i) => (
-              <li key={`${i}_${filterItem.name}`}>
-                <button onClick={() => onSelectFilter(filterItem)}>
-                  {django.gettext(filterItem.name)}
-                </button>
-              </li>
-            ))}
-          </ul>
-        </div>
+    <div className="mt-2 mt-sm-0">
+      <div className="dropdown dropdown-menu-end">
+        <button
+          type="button"
+          className="dropdown-toggle btn btn--light btn--select"
+          data-bs-toggle="dropdown"
+          aria-expanded="false"
+        >
+          {django.gettext('Filter')}: {currFilterItem.label}
+          <i className="fa fa-caret-down" aria-hidden="true" />
+        </button>
+        <ul className="dropdown-menu">
+          {props.filterItems.map((filterItem, i) => (
+            <li key={`${i}_${filterItem.label}`}>
+              <button onClick={() => onSelectFilter(filterItem)}>
+                {django.gettext(filterItem.label)}
+              </button>
+            </li>
+          ))}
+        </ul>
       </div>
     </div>
   )
