248d32eac8af3bfdcb8d890739fb2ffe53aceac7
Kha <k.tran@liqd.net>
2022-05-09 13:30:11 +0100

    apps/userdashboard/moderationNotification/ActionsBar/HoverButton: refactor to show hoverbutton to have less complexity
    - tabbing possible
    - disabling button while loading/refetching

--- apps/contrib/assets/HoverButton.jsx
+++ apps/contrib/assets/HoverButton.jsx
@@ -1,21 +1,31 @@
 import React, { useState, useEffect } from 'react'
 
 export const HoverButton = (props) => {
-  const [buttonText, setButtonText] = useState(props.text)
+  const { textMouseOn, textMouseOff, onClick } = props
+  const [buttonText, setButtonText] = useState(textMouseOff)
+  const [processing, setProcessing] = useState(false)
+
+  const handleClick = () => {
+    setProcessing(true)
+    onClick()
+  }
 
   useEffect(() => {
-    setButtonText(props.text)
-  }, [props.text])
+    setProcessing(false)
+    setButtonText(textMouseOff)
+  }, [textMouseOff])
 
   return (
     <button
       id={props.id}
       className={props.className}
       type="button"
-      onClick={props.onClick}
-      disabled={props.disabled}
-      onMouseEnter={() => setButtonText(props.hoverText)}
-      onMouseLeave={() => setButtonText(props.text)}
+      onClick={handleClick}
+      disabled={processing || props.disabled}
+      onMouseEnter={() => setButtonText(textMouseOn)}
+      onMouseLeave={() => setButtonText(textMouseOff)}
+      onFocus={() => setButtonText(textMouseOn)}
+      onBlur={() => setButtonText(textMouseOff)}
       aria-label={buttonText}
     >
       {props.icon}
