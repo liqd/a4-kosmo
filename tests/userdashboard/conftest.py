1893db078da27e3812624559c1da45de13b14a77
fuzzylogic2000 <k.lindenlaub@liqd.net>
2022-05-13 14:53:14 +0200

    tests/userdashboard: test ordering filter

--- tests/userdashboard/conftest.py
+++ tests/userdashboard/conftest.py
@@ -2,7 +2,9 @@ from pytest_factoryboy import register
 
 from tests.classifications import factories
 from tests.ideas.factories import IdeaFactory
+from tests.moderatorfeedback.factories import ModeratorCommentStatementFactory
 
 register(IdeaFactory)
 register(factories.UserClassificationFactory)
 register(factories.AIClassificationFactory)
+register(ModeratorCommentStatementFactory)
