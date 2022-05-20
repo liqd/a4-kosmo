38968adee252e997e933845e4bc322925d144f35
rine <k.schmid@liqd.net>
2022-05-20 15:50:42 +0200

    tests/classifications: add tests for classifications exports

--- tests/classifications/conftest.py
+++ tests/classifications/conftest.py
@@ -1,9 +1,11 @@
 from pytest_factoryboy import register
 
 from tests.ideas import factories as idea_factories
+from tests.moderatorfeedback.factories import ModeratorCommentStatementFactory
 
 from . import factories
 
 register(idea_factories.IdeaFactory)
 register(factories.UserClassificationFactory)
 register(factories.AIClassificationFactory)
+register(ModeratorCommentStatementFactory)
