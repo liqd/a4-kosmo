81bb43e92785a8c6d083fc544bf382ea7cde677b
rine <k.schmid@liqd.net>
2022-05-24 12:48:02 +0200

    classifications/userdashboard: adapt counts to single choice classifications, also fixes #245

--- apps/classifications/serializers.py
+++ apps/classifications/serializers.py
@@ -5,32 +5,32 @@ from apps.classifications.models import UserClassification
 
 
 class UserClassificationSerializer(serializers.ModelSerializer):
-    classifications = serializers.SerializerMethodField()
+    classification = serializers.SerializerMethodField()
     created = serializers.SerializerMethodField()
 
     class Meta:
         model = UserClassification
-        fields = ['classifications', 'comment_text',
+        fields = ['classification', 'comment_text',
                   'created', 'is_pending', 'pk', 'user_message']
 
-    def get_classifications(self, instance):
-        return instance.get_classifications_list()
+    def get_classification(self, instance):
+        return instance.get_classification_display()
 
     def get_created(self, instance):
         return instance.created.strftime('%d.%m.%y')
 
 
 class AIClassificationSerializer(serializers.ModelSerializer):
-    classifications = serializers.SerializerMethodField()
+    classification = serializers.SerializerMethodField()
     created = serializers.SerializerMethodField()
 
     class Meta:
         model = AIClassification
-        fields = ['classifications', 'comment_text',
+        fields = ['classification', 'comment_text',
                   'created', 'is_pending', 'pk']
 
-    def get_classifications(self, instance):
-        return instance.get_classifications_list()
+    def get_classification(self, instance):
+        return instance.get_classification_display()
 
     def get_created(self, instance):
         return instance.created.strftime('%d.%m.%y')
