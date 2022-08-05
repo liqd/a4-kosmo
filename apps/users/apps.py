1e7945c1f376e570bb5d7b3c0806b5b0d7943f5a
rine <k.schmid@liqd.net>
2022-08-05 18:49:13 +0200

    users: auto follow sample project upon registration

--- apps/users/apps.py
+++ apps/users/apps.py
@@ -2,5 +2,8 @@ from django.apps import AppConfig
 
 
 class Config(AppConfig):
-    name = "apps.users"
-    label = "a4_candy_users"
+    name = 'apps.users'
+    label = 'a4_candy_users'
+
+    def ready(self):
+        import apps.users.signals  # noqa:F401
