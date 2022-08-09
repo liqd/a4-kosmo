7e41a09d93ca3b705cbb972abe2e5f0002a75e69
fuzzylogic2000 <k.lindenlaub@liqd.net>
2022-08-09 16:13:20 +0200

    apps/user: send send in blue welcome email after email confirm

--- apps/users/signals.py
+++ apps/users/signals.py
@@ -1,9 +1,12 @@
+from allauth.account.signals import email_confirmed
 from allauth.account.signals import user_signed_up
 from django.dispatch import receiver
 
 from adhocracy4.follows.models import Follow
 from adhocracy4.projects.models import Project
 from apps.cms.settings.models import OrganisationSettings
+
+from . import emails
 
 
 @receiver(user_signed_up)
@@ -16,3 +19,10 @@ def auto_follow_sample_project(request, user, **kwargs):
             Follow.objects.create(creator=user, project=sample_project)
         except Project.DoesNotExist:
             pass
+
+
+@receiver(email_confirmed)
+def send_welcome_email(request, email_address, **kwargs):
+    user = email_address.user
+    if user.emailaddress_set.all().count() == 1:
+        emails.WelcomeEmail.send(user)
