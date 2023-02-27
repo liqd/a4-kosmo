22d37a240eb598df625900e3a880d22b2f4df579
Phillippa <phillippa@PhillipasLaptop.fritz.box>
2023-02-27 11:50:08 +0100

    apps/projects: changes for userdashboard

--- tests/projects/conftest.py
+++ tests/projects/conftest.py
@@ -1,6 +1,13 @@
 from pytest_factoryboy import register
 
+from tests.classifications import factories as classification_factories
+from tests.ideas import factories as idea_factories
+
 from . import factories as invites
 
 register(invites.ModeratorInviteFactory)
 register(invites.ParticipantInviteFactory)
+
+register(classification_factories.AIClassificationFactory)
+register(classification_factories.UserClassificationFactory)
+register(idea_factories.IdeaFactory)
