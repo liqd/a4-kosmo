197b4a74a49d9655fb73498a3a60f1c9067f6f97
Phillippa <phillippa@PhillipasLaptop.fritz.box>
2023-02-27 11:54:15 +0100

    apps/moderatorfeedback: add moderatorfeedback to comments

---
+++ apps/moderatorfeedback/api.py
@@ -0,0 +1,36 @@
+from rest_framework import mixins
+from rest_framework import viewsets
+
+from adhocracy4.api.mixins import CommentMixin
+from adhocracy4.api.permissions import ViewSetRulesPermission
+from adhocracy4.comments_async import api as a4_api
+from apps.moderatorfeedback.models import ModeratorCommentStatement
+from apps.moderatorfeedback.serializers import \
+    ModeratorCommentStatementSerializer
+from apps.moderatorfeedback.serializers import ThreadListSerializer
+from apps.moderatorfeedback.serializers import ThreadSerializer
+
+
+class ModeratorCommentStatementViewSet(CommentMixin,
+                                       mixins.CreateModelMixin,
+                                       mixins.DestroyModelMixin,
+                                       mixins.UpdateModelMixin,
+                                       mixins.ListModelMixin,
+                                       viewsets.GenericViewSet):
+    serializer_class = ModeratorCommentStatementSerializer
+    permission_classes = (ViewSetRulesPermission,)
+
+    def get_permission_object(self):
+        return self.comment
+
+    def get_queryset(self):
+        return ModeratorCommentStatement.objects.filter(
+            comment=self.comment)
+
+
+class CommentWithStatementViewSet(a4_api.CommentViewSet):
+
+    def get_serializer_class(self):
+        if self.action == 'list':
+            return ThreadListSerializer
+        return ThreadSerializer
