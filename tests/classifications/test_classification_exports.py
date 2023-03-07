9e8e6a1b57c57d82458c8cb5cee652cb7c89eeda
fuzzylogic2000 <k.lindenlaub@liqd.net>
2023-03-07 16:18:56 +0100

    apps/moderatorfeedback: add another merge migration (after change of TimeStampedModel fields) to make sure all migrations are run

--- tests/classifications/test_classification_exports.py
+++ tests/classifications/test_classification_exports.py
@@ -5,28 +5,38 @@ from apps.classifications.exports import UserClassificationExport
 
 
 @pytest.mark.django_db
-def test_ai_classifications_export(idea, comment_factory,
-                                   ai_classification_factory,
-                                   moderator_comment_statement_factory):
-
+def test_ai_classifications_export(
+    idea,
+    comment_factory,
+    ai_classification_factory,
+    moderator_comment_feedback_factory,
+):
     comment_1 = comment_factory(content_object=idea)
     comment_2 = comment_factory(content_object=idea)
-    moderator_statement = moderator_comment_statement_factory(
-        comment=comment_1)
+    moderator_feedback = moderator_comment_feedback_factory(comment=comment_1)
     ai_classification_1 = ai_classification_factory(comment=comment_1)
     ai_classification_2 = ai_classification_factory(
-        comment=comment_2,
-        classification='ENGAGING')
+        comment=comment_2, classification="ENGAGING"
+    )
     ai_classification_3 = ai_classification_factory(comment=comment_1)
 
     ai_classification_export = AIClassificationExport()
 
     header = ai_classification_export.get_header()
-    assert header == ['ID', 'created', 'classification', 'comment text',
-                      'is pending', 'comment_id', 'comment_comment',
-                      'comment_is_blocked', 'comment_is_moderator_marked',
-                      'comment_created', 'comment_modified',
-                      'comment_moderator_statement']
+    assert header == [
+        "ID",
+        "Created",
+        "classification",
+        "comment text",
+        "is pending",
+        "comment_id",
+        "comment_comment",
+        "comment_is_blocked",
+        "comment_is_moderator_marked",
+        "comment_created",
+        "comment_modified",
+        "comment_moderator_feedback",
+    ]
 
     queryset = ai_classification_export.get_queryset()
     assert queryset.count() == 3
@@ -35,38 +45,56 @@ def test_ai_classifications_export(idea, comment_factory,
     assert ai_classification_3 in queryset
 
     assert ai_classification_export.get_field_data(
-        ai_classification_1, 'comment_id') == str(comment_1.id)
+        ai_classification_1, "comment_id"
+    ) == str(comment_1.id)
+    assert (
+        ai_classification_export.get_field_data(ai_classification_2, "classification")
+        == "ENGAGING"
+    )
     assert ai_classification_export.get_field_data(
-        ai_classification_2, 'classification') == 'ENGAGING'
-    assert ai_classification_export.get_field_data(
-        ai_classification_1, 'comment_moderator_statement') == \
-        str(moderator_statement)
-    assert ai_classification_export.get_field_data(
-        ai_classification_2, 'comment_moderator_statement') == ''
+        ai_classification_1, "comment_moderator_feedback"
+    ) == str(moderator_feedback)
+    assert (
+        ai_classification_export.get_field_data(
+            ai_classification_2, "comment_moderator_feedback"
+        )
+        == ""
+    )
 
 
 @pytest.mark.django_db
-def test_user_classifications_export(idea, comment_factory,
-                                     user_classification_factory,
-                                     moderator_comment_statement_factory):
-
+def test_user_classifications_export(
+    idea,
+    comment_factory,
+    user_classification_factory,
+    moderator_comment_feedback_factory,
+):
     comment_1 = comment_factory(content_object=idea)
     comment_2 = comment_factory(content_object=idea)
-    moderator_statement = moderator_comment_statement_factory(
-        comment=comment_1)
+    moderator_feedback = moderator_comment_feedback_factory(comment=comment_1)
     user_classification_1 = user_classification_factory(comment=comment_1)
     user_classification_2 = user_classification_factory(
-        comment=comment_2,
-        user_message='this needs to be reported')
+        comment=comment_2, user_message="this needs to be reported"
+    )
 
     user_classification_export = UserClassificationExport()
 
     header = user_classification_export.get_header()
-    assert header == ['ID', 'created', 'classification', 'comment text',
-                      'is pending', 'comment_id', 'comment_comment',
-                      'comment_is_blocked', 'comment_is_moderator_marked',
-                      'comment_created', 'comment_modified',
-                      'comment_moderator_statement', 'user message']
+    assert header == [
+        "ID",
+        "Created",
+        "classification",
+        "comment text",
+        "is pending",
+        "comment_id",
+        "comment_comment",
+        "comment_is_blocked",
+        "comment_is_moderator_marked",
+        "comment_created",
+        "comment_modified",
+        "comment_moderator_feedback",
+        "user message",
+    ]
 
     queryset = user_classification_export.get_queryset()
     assert queryset.count() == 2
@@ -74,13 +102,22 @@ def test_user_classifications_export(idea, comment_factory,
     assert user_classification_2 == queryset.last()
 
     assert user_classification_export.get_field_data(
-        user_classification_1, 'comment_id') == str(comment_1.id)
+        user_classification_1, "comment_id"
+    ) == str(comment_1.id)
+    assert (
+        user_classification_export.get_field_data(user_classification_1, "user_message")
+        == "This is bad."
+    )
+    assert (
+        user_classification_export.get_field_data(user_classification_2, "user_message")
+        == "this needs to be reported"
+    )
     assert user_classification_export.get_field_data(
-        user_classification_1, 'user_message') == 'This is bad.'
-    assert user_classification_export.get_field_data(
-        user_classification_2, 'user_message') == 'this needs to be reported'
-    assert user_classification_export.get_field_data(
-        user_classification_1, 'comment_moderator_statement') == \
-        str(moderator_statement)
-    assert user_classification_export.get_field_data(
-        user_classification_2, 'comment_moderator_statement') == ''
+        user_classification_1, "comment_moderator_feedback"
+    ) == str(moderator_feedback)
+    assert (
+        user_classification_export.get_field_data(
+            user_classification_2, "comment_moderator_feedback"
+        )
+        == ""
+    )
